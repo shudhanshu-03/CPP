3e4ab797cbe93be6de2451074b8cd165faf2224f
shudhanshu03 <shudhanshuprajapati1234@gmail.com>
2025-02-06 00:14:56 +0530

    first commit

--- Conditional_statements/Sum_of_first_10_natural_numbers.cpp
+++ Conditional_statements/Sum_of_first_10_natural_numbers.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main(){
     int i, sum=0;
-    for(int i=1;i<=10;i++){
+    for(int i=1; i<=10; i++){
         sum+=i;
     }
     cout<<"Sum of first ten natural numbers is: "<<sum<<endl;
