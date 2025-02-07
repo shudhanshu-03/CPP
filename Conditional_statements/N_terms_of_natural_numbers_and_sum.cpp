4e2573c6f6bf7609a180d39e7397ea73d8797d1b
shudhanshu03 <shudhanshuprajapati1234@gmail.com>
2025-02-07 23:29:15 +0530

    first commit

---
+++ Conditional_statements/N_terms_of_natural_numbers_and_sum.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n, sum=0;
+    for(int i=0; i<n; i++){
+        sum+=i;
+    }
+    cout<<"Sum of first "<<n<<" natural numbers is: "<<sum<<endl;
+}
